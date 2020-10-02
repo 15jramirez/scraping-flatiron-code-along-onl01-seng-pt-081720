a7707568b9c51cb89a6c9496f8bfcd9db9c09ffe
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-10-02 03:21:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,7 +10,7 @@ class Scraper
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
   
-  def get_course
+  def get_courses
      self.get_page.css(".post")
   end
   
@@ -40,4 +40,3 @@ end
 
 
 Scraper.new.get_page
-Scraper.new.print_courses
